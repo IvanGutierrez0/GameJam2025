ba800ef0adebefe286197b719d1d8749cd7b623b
IvanGutierrez0 <ivangutierrezgarcia0@gmail.com>
2025-07-06 02:24:54 +0200

    Bat finished

--- Scripts/bat.gd
+++ Scripts/bat.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 @export var path = [Vector2(100, 100), Vector2(300, 100), Vector2(300, 300), Vector2(100, 300)]
-var speed = 100
+@export var speed = 100
+@export var followDistance = 200
 var current_point = 0
 var initialPos
 
@@ -16,12 +17,18 @@ func _ready() -> void:
 	player = get_node("../../Player")
 
 func _process(delta):
-	var target = path[current_point] + initialPos
-	var direction = (target - position).normalized()
-	position += direction * speed * delta
+	var direction
+	
+	if position.distance_to(player.position) < followDistance:
+		direction = (player.position - position).normalized()
+		position += direction * speed * delta
+	else:
+		var target = path[current_point] + initialPos
+		direction = (target - position).normalized()
+		position += direction * speed * delta
 
-	if position.distance_to(target) < 5:
-		current_point = (current_point + 1) % path.size()
+		if position.distance_to(target) < 5:
+			current_point = (current_point + 1) % path.size()
 	
 	$AnimatedSprite2D.flip_h = direction.x > 0
 
