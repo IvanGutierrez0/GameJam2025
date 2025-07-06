aa3a952eec0062377ecba9bc83449e8a5835d245
IvanGutierrez0 <ivangutierrezgarcia0@gmail.com>
2025-07-06 04:42:25 +0200

    Throwing animation done

--- Scripts/player_cb.gd
+++ Scripts/player_cb.gd
@@ -23,6 +23,7 @@ func _start() -> void:
 func _ready():
 	main = get_node("../")
 	$DynamicLight.energy = lightEnergy
+	$AnimatedSprite2D.animation = "idle"
 	$AnimatedSprite2D.flip_h = true
 	$Flash.hide()
 
@@ -40,18 +41,21 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_v = false
-		$AnimatedSprite2D.flip_h = velocity.x > 0
-	if not is_on_floor():
-		if velocity.y > 0:
-			$AnimatedSprite2D.animation = "jumpDown"
-		else:
-			$AnimatedSprite2D.animation = "jumpUp"
+	if $AnimatedSprite2D.animation != "throw":
+		if velocity.x != 0:
+			$AnimatedSprite2D.animation = "walk"
+			$AnimatedSprite2D.flip_v = false
+		if not is_on_floor():
+			if velocity.y > 0:
+				$AnimatedSprite2D.animation = "jumpDown"
+			else:
+				$AnimatedSprite2D.animation = "jumpUp"
+		
+		if velocity.length() == 0:
+			$AnimatedSprite2D.animation = "idle"
 	
-	if velocity.length() == 0:
-		$AnimatedSprite2D.animation = "idle"
+	if velocity.x != 0:
+		$AnimatedSprite2D.flip_h = velocity.x > 0
 	
 	$AnimatedSprite2D.play()
 	move_and_slide()
@@ -61,17 +65,20 @@ func _physics_process(delta: float) -> void:
 		$Flash.modulate.a = transparency
 	
 	if Input.is_action_pressed("useHab") and $Timers/PowerTimer.is_stopped():
-		spawnFlashbang(lastDirection) 
-		$Timers/PowerTimer.start()
+		$AnimatedSprite2D.animation = "throw"
+		$AnimatedSprite2D.play()
 
 func spawnFlashbang(direction) -> void:
 	flashbang = flashbang_scene.instantiate()
 	flashbang.position.x = global_position.x + 10 * direction
-	flashbang.position.y = global_position.y + 5
+	flashbang.position.y = global_position.y - 18  
 	flashbang.linear_velocity = Vector2(250 * direction + velocity.x, -250)
 	
 	projectiles_container = get_tree().get_root().get_node("Main/Projectiles")
 	projectiles_container.add_child(flashbang)
+	
+	$AnimatedSprite2D.animation = "idle"
+	$Timers/PowerTimer.start()
 
 func _on_inmunity_timer_timeout() -> void:
 	$Timers/InmunityTimer.stop()
@@ -110,3 +117,8 @@ func _on_flashed_timer_timeout() -> void:
 	$Timers/FlashedTimer.stop()
 	$Flash.hide()
 	transparency = 0
+		
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if $AnimatedSprite2D.animation == "throw":
+		spawnFlashbang(lastDirection) 
