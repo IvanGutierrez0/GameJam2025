b6299202f9eecf804df8a3ace278cefcc2168eb0
IvanGutierrez0 <ivangutierrezgarcia0@gmail.com>
2025-07-06 12:14:12 +0200

    Candles done

---
+++ Scripts/goal.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var nextLevel = ""
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimatedSprite2D.animation = "off"
+	$AnimatedSprite2D.play()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		$AnimatedSprite2D.animation = "lit"
+		$AnimatedSprite2D.play()
+		$Speaker.play()
+
+func _on_speaker_finished() -> void:
+	get_tree().change_scene_to_file(nextLevel)
